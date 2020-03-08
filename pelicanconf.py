db4a0fa15218b848fe4f9557e1c1b74a21bb0f63
Grape Ape <th3grap3ap3@gmail.com>
2020-03-08 17:06:19 -0500

    Updated links in config.

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,11 +23,13 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('RedLegg', 'https://redlegg.com'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/grap3_ap3'),
+          ('Instagram', 'https://www.instagram.com/phil_grimes/?hl=en'),
+          ('LinkedIn', 'https://www.linkedin.com/in/phil-grimes-a4486615/'),
+          ('GitHub', 'https://github.com/grap3-ap3')
 
 DEFAULT_PAGINATION = 10
 
